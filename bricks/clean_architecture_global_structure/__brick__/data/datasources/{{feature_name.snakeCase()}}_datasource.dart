3ad9620d2f5a75dec0527cc7ea15abc6f4affd19
Jonatas Brito <jonatas.furtuna@gmail.com>
2022-08-31 12:47:05 -0300

    feat(Data layer): create data source file
    Create the data source file using the feature_name variable in your path and in class name.

---
+++ bricks/clean_architecture_global_structure/__brick__/data/datasources/{{feature_name.snakeCase()}}_datasource.dart
@@ -0,0 +1,7 @@
+abstract class {{feature_name.pascalCase()}}DataSource {
+  // TODO: implement your {{feature_name.pascalCase()}}DataSource
+}
+
+class {{feature_name.pascalCase()}}DataSourceImpl implements {{feature_name.pascalCase()}}DataSource {
+  // TODO: implement your {{feature_name.pascalCase()}}DataSourceImpl
+}
