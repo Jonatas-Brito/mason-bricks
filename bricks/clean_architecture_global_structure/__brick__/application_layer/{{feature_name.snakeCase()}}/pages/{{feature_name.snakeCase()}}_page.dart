1fa8ed2559f95f38d8ab50910257fbe2b86c158a
Jonatas Brito <jonatas.furtuna@gmail.com>
2022-08-31 13:28:12 -0300

    feat(Application layer): create a page file
    Create page file using the feature_name variable in your path and in page name.

---
+++ bricks/clean_architecture_global_structure/__brick__/application_layer/{{feature_name.snakeCase()}}/pages/{{feature_name.snakeCase()}}_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class {{feature_name.pascalCase()}}Page extends StatelessWidget {
+  const {{feature_name.pascalCase()}}Page ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox();
+  }
+}
