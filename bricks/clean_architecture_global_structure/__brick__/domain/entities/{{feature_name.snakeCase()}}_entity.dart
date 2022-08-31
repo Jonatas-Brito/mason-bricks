498e519eb79a1597969bcc4468acb352814518a0
Jonatas Brito <jonatas.furtuna@gmail.com>
2022-08-31 13:03:42 -0300

    feat(Domain layer): create a entity
    Create entity file using the feature_name variable in your path and in entity class name.

---
+++ bricks/clean_architecture_global_structure/__brick__/domain/entities/{{feature_name.snakeCase()}}_entity.dart
@@ -0,0 +1,9 @@
+import 'package:equatable/equatable.dart';
+
+class {{feature_name.pascalCase()}}Entity extends Equatable {
+  // TODO: implement your {{feature_name.pascalCase()}}Entity
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
+}
