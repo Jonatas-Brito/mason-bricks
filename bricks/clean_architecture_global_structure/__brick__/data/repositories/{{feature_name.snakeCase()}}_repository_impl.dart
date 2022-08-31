83b0a2d78635a0caf41b1a38bd9bc85f982ee9e6
Jonatas Brito <jonatas.furtuna@gmail.com>
2022-08-31 12:59:11 -0300

    feat(Data layer): create a repository implementation
    Create repository implementation file using the feature_name variable in your path and in repository class name.

---
+++ bricks/clean_architecture_global_structure/__brick__/data/repositories/{{feature_name.snakeCase()}}_repository_impl.dart
@@ -0,0 +1,5 @@
+import "package:{{{package_name}}}/domain/repositories/{{feature_name.snakeCase()}}_repository.dart";
+
+class {{feature_name.pascalCase()}}RepositoryImpl implements {{feature_name.pascalCase()}}Repository {
+  // TODO: implement your {{feature_name.pascalCase()}}RepositoryImpl
+}
