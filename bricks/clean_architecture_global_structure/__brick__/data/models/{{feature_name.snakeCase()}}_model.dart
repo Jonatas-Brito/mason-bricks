45a236c85da21a8e67d9375952659ae73eaa2c97
Jonatas Brito <jonatas.furtuna@gmail.com>
2022-08-31 12:54:06 -0300

    feat(Data layer): create a model file
    Create model file using the feature_name variable in your path and in model class name.

---
+++ bricks/clean_architecture_global_structure/__brick__/data/models/{{feature_name.snakeCase()}}_model.dart
@@ -0,0 +1,5 @@
+import 'package:{{{package_name}}}/domain/entities/{{feature_name.snakeCase()}}_entity.dart';
+
+class {{feature_name.pascalCase()}}Model extends {{feature_name.pascalCase()}}Entity {
+  // TODO: implement your {{feature_name.pascalCase()}}Model
+}
